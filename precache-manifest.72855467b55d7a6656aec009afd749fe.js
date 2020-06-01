bbbbd2a48927cbdea68c98102fd054440622af35
Zehao <a15zehcheche@inspedralbes.cat>
2020-06-01 14:46:11 +0200

    Updates

---
+++ precache-manifest.72855467b55d7a6656aec009afd749fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d172005cbfa3e1221ee67538139e23c",
+    "url": "/raconet/index.html"
+  },
+  {
+    "revision": "9679fc711ba5d4921ae6",
+    "url": "/raconet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "643157b685388adfadb5",
+    "url": "/raconet/static/js/2.d170713a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/raconet/static/js/2.d170713a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9679fc711ba5d4921ae6",
+    "url": "/raconet/static/js/main.1fa33b0f.chunk.js"
+  },
+  {
+    "revision": "203ea9fb2382321bb52f",
+    "url": "/raconet/static/js/runtime-main.ee003eac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/raconet/static/media/logo.5d5d9eef.svg"
+  }
+]);
